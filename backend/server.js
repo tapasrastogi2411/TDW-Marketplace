607ff1836207c3b610b6a504c0ec7163da4800f4
winsyuan <winsonyua@gmail.com>
2022-08-02 20:59:05 -0400

    chore: update deployment config

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,7 @@ const redisConnection = {
   connection: {
     host: process.env.REDIS_HOST || "redis",
     port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD,
   },
 };
 
